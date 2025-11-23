4c9b2f0a2947c1d590db27032bf19ebafccb72e3
dperez <daniel.perezalonso23@gmail.com>
2025-11-23 23:21:33 +0100

    versio2

--- sumaprod.c
+++ sumaprod.c
@@ -4,6 +4,12 @@
 
 //funcio principal
 int main (int argc, char **argv) {
+
+//comprovador
+if (argc != 4) {
+    printf("Proporciona 3 arguments: operador num1 num2\n");
+    return 1;
+}
     //comproba si es una suma
     if (!strcmp (argv[1], "+"))
         printf ("La suma de %s i %s es %d\n", argv[2], argv[3], atoi(argv[2]) + atoi(argv[3]));
